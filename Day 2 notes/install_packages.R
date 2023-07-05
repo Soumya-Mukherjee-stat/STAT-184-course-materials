8c070e29ae32e8fc262a0dfe566eb4f918fc8553
Soumya-Mukherjee-stat <smukherjeestat@gmail.com>
2023-07-04 22:58:07 -0400

    Day 2 materials added

---
+++ Day 2 notes/install_packages.R
@@ -0,0 +1,5 @@
+install.packages("devtools")
+library(devtools)
+devtools::install_github("DataComputing/DataComputing")
+install.packages(c("tidyverse","knitr","rmarkdown","rpart","shiny","manipulate"))
+install.packages(c("mosaic","mosaicData","NHANES"))
